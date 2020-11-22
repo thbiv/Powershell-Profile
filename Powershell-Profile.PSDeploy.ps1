ae1cd55e53d1777bc2a3ae9ccebedee19d56b0f2
Thomas Barratt <thomas.barratt@gmail.com>
2020-11-22 11:05:03 -0500

    PSScriptTools and filesystem-ansi formating

--- Powershell-Profile.PSDeploy.ps1
+++
@@ -1,7 +0,0 @@
-Deploy PowerhellProfiles {
-    By FileSystem CurrentUserConsoleHost {
-        FromSource '_output\CurrentUserConsoleHost.ps1'
-        To '\\localhost\c$\Users\thbarratt\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1',
-           '\\localhost\c$\Users\thbarratt\Documents\PowerShell\Microsoft.PowerShell_profile.ps1'
-    }
-}
--- source/CurrentUserConsoleHost.ps1
+++ source/CurrentUserConsoleHost.ps1
@@ -1,5 +1,5 @@
 # Import Modules
-Import-Module -Name PSReadLine, posh-git
+Import-Module -Name PSReadLine, posh-git, PSScriptTools
 Import-Module -Name Pscx -Function help, less, Show-Tree, Start-PowerShell -Cmdlet ConvertFrom-Base64, ConvertTo-Base64
 Import-Module -Name PowerShellCookbook -Function Show-Object
 
@@ -132,30 +132,9 @@ Function Convert-SIDToUserName {
     ((((New-Object System.Security.Principal.SecurityIdentifier($SID)).Translate([System.Security.Principal.NTAccount])).Value).Split('\'))[1]
 }
 
-# Function to color code the Get-ChildItem output depending on extention of the file.
-Function Get-ColoredDir {
-    Param ($dir = ".", $all = $false)
-    $origFg = $host.ui.rawui.foregroundColor
-    If ($all) {$toList = Get-ChildItem -force $dir}
-    Else {$toList = Get-ChildItem $dir}
-    ForEach ($Item in $toList) {
-        Switch ($Item.Extension) {
-            ".Exe" {$host.ui.rawui.foregroundColor = "Green"}
-            ".cmd" {$host.ui.rawui.foregroundColor = "DarkGreen"}
-            ".bat" {$host.ui.rawui.foregroundColor = "DarkGreen"}
-            ".ps1" {$host.ui.rawui.foregroundColor = "Magenta"}
-            ".vbs" {$host.ui.rawui.foregroundColor = "DarkGreen"}
-            ".psm1" {$host.ui.rawui.foregroundColor = "Magenta"}
-            ".psd1" {$host.ui.rawui.foregroundColor = "Magenta"}
-            ".txt" {$host.ui.rawui.foregroundColor = "Yellow"}
-            Default {$host.ui.rawui.foregroundColor = $origFg}
-        }
-        If ($item.Mode.StartsWith("d")) {$host.ui.rawui.foregroundColor = $origFg}
-        $item
-    }
-    $host.ui.rawui.foregroundColor = $origFg
-}
-New-Alias -Name LL -Value Get-ColoredDir
+# Color Get-ChildItem filesystem Output using PSScriptTools module
+$FormatFile = (Get-Module PSScriptTools).ExportedFormatFiles | where-object {$_ -match 'filesystem-ansi'}
+Update-FormatData -PrependPath $FormatFile
 
 # Function to invoke the install-powershell.ps1 script to install the latest version of powershell
 Function Install-Powershell {

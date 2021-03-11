7326c8da814c762f3ab23f2d376fffd7e9661b43
Thomas Barratt <thomas.barratt@gmail.com>
2021-03-11 18:55:59 -0500

    update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,7 +1,16 @@
-[CmdletBinding()]
-Param (
-	[switch]$Force
-)
+Function _InstallProfile {
+	$URL = $Response.assets.browser_download_url
+	Invoke-WebRequest -Uri $URL -OutFile "$OutputPath\$($Response.assets.name)"
+	Expand-Archive -Path "$OutputPath\$($Response.assets.name)" -DestinationPath "$OutputPath" -Force -ErrorAction 'Stop'
+	Move-Item -Path "$OutputPath\Profile.ps1" -Destination $ProfilePath -ErrorAction 'Stop' -Force
+}
+
+Function _InstallPSR {
+	$URL = $Response.assets.browser_download_url
+	Invoke-WebRequest -Uri $URL -OutFile "$OutputPath\$($Response.assets.name)"
+	Expand-Archive -Path "$OutputPath\$($Response.assets.name)" -DestinationPath "$OutputPath" -Force -ErrorAction 'Stop'
+	Move-Item -Path "$OutputPath\PSReadline-Profile.ps1" -Destination "$PSRPath" -ErrorAction 'Stop' -Force
+}
 
 If ($IsWindows) {
 	$OutputPath = $Env:temp
@@ -11,8 +20,14 @@ If ($IsWindows) {
 
 $ProfileIncludePath = "$(Split-Path -Path $($Profile.CurrentUserAllHosts) -Parent)\ProfileInclude"
 If (-not(Test-Path -Path $ProfileIncludePath)) {
+	Write-Host "ProfileInclude folder does not exist"
     New-Item -Path $ProfileIncludePath -ItemType Directory -Force | Out-Null
+} Else {
+	Write-Host "ProfileInclude folder already exists"
 }
+
+$ProfilePath = "$($Profile.CurrentUserAllHosts)"
+$PSRPath = "$ProfileIncludePath\PSReadline-Profile.ps1"
 
 $PorfileParams = @{
 	'Uri' = 'https://api.github.com/repos/thbiv/Powershell-Profile/releases/latest'
@@ -21,15 +36,18 @@ $PorfileParams = @{
 	'UseBasicParsing' = $True
 }
 $Response = Invoke-RestMethod @PorfileParams
-$InstalledProfileBuild = (((Get-Content $Profile.CurrentUserAllHosts)[2] -split ':')[1]).TrimStart()
-If (($InstalledProfileBuild -ne $($Response.tag_name)) -or $Force) {
-	$URL = $Response.assets.browser_download_url
-	Invoke-WebRequest -Uri $URL -OutFile "$OutputPath\$($Response.assets.name)"
-	Expand-Archive -Path "$OutputPath\$($Response.assets.name)" -DestinationPath "$OutputPath" -Force -ErrorAction 'Stop'
-	Move-Item -Path "$OutputPath\Profile.ps1" -Destination $($Profile.CurrentUserAllHosts) -ErrorAction 'Stop' -Force
-	Write-Host "[Powershell-Profile] Installed Latest Release: $($Response.name)"
+
+If (Test-Path -Path $ProfilePath) {
+	$InstalledProfileBuild = (((Get-Content $ProfilePath)[2] -split ':')[1]).TrimStart()
+	If ($InstalledProfileBuild -ne $($Response.tag_name)) {
+		_InstallProfile
+		Write-Host "[Powershell-Profile] Upgraded to Latest Release: $($Response.name)"
+	} Else {
+		Write-Host "[Powershell-Profile] Latest Release is already installed"
+	}
 } Else {
-	Write-Host "[Powershell-Profile] Latest Release is already installed"
+	_InstallProfile
+	Write-Host "[Powershell-Profile] Installed Latest Release: $($Response.name)"
 }
 
 $PSRParams = @{
@@ -39,13 +57,18 @@ $PSRParams = @{
 	'UseBasicParsing' = $True
 }
 $Response = Invoke-RestMethod @PSRParams
-$InstalledPSRBuild = (((Get-Content "$ProfileIncludePath\PSReadline-Profile.ps1")[2] -split ':')[1]).TrimStart()
-If (($InstalledPSRBuild -ne $($Response.tag_name)) -or $Force) {
-	$URL = $Response.assets.browser_download_url
-	Invoke-WebRequest -Uri $URL -OutFile "$OutputPath\$($Response.assets.name)"
-	Expand-Archive -Path "$OutputPath\$($Response.assets.name)" -DestinationPath "$OutputPath" -Force -ErrorAction 'Stop'
-	Move-Item -Path "$OutputPath\PSReadline-Profile.ps1" -Destination "$(Split-Path -Path $($Profile.CurrentUserAllHosts) -Parent)\ProfileInclude\PSReadline-Profile.ps1" -ErrorAction 'Stop' -Force
-	Write-Host "[PSReadline-Profile] Installed Latest Release: $($Response.name)"
+
+If (Test-Path -Path $PSRPath) {
+	$InstalledPSRBuild = (((Get-Content $PSRPath)[2] -split ':')[1]).TrimStart()
+	If ($InstalledPSRBuild -ne $($Response.tag_name)) {
+		_InstallPSR
+		Write-Host "[PSReadline-Profile] Upgraded to Latest Release: $($Response.name)"
+	} Else {
+		Write-Host "[PSReadline-Profile] Latest Release is already installed"
+	}
 } Else {
-	Write-Host "[PSReadline-Profile] Latest Release is already installed"
+	_InstallPSR
+	Write-Host "[PSReadline-Profile] Installed Latest Release: $($Response.name)"
 }
+
+
